57993952081771555108c80e8843c42a8a7975c6
Anoop <Anooppopziyan@gmail.com>
2025-12-01 12:11:31 +0530

    optimized 1.two sum

--- TwoSum.java
+++ TwoSum.java
@@ -1,31 +1,37 @@
-public class TwoSum{
-    public static void main(String[] args) {
-        Solution s1=new Solution();
-        int[] case1=s1.twoSum(new int[]{7,2,5,11,15}, 9);
-        System.out.println(case1[0]+","+case1[1]);
-
-        Solution s2=new Solution();
-        int[] case2=s2.twoSum(new int[]{7,2,5,11,15}, 7);
-        System.out.println(case2[0]+","+case2[1]);
-
-        
-        Solution s3=new Solution();
-        int[] case3=s3.twoSum(new int[]{7,2,5,11,15}, 26);
-        System.out.println(case3[0]+","+case3[1]);
-
-
-    }
-}
+/**
+ * 1. Two Sum
+ * 
+ * Approach: HashMap (One-pass)
+ *
+ * We iterate through the array once and use a HashMap to store:
+ *    key   -> number in the array
+ *    value -> index of that number
+ *
+ * For each number nums[i], we compute its complement:
+ *        complement = target - nums[i]
+ *
+ * If the complement already exists in the map, it means we have previously
+ * seen a number that can pair with nums[i] to form the target sum.
+ * In that case, we immediately return both indices.
+ *
+ * Otherwise, we store the current number and its index in the map and continue.
+ *
+ * Time Complexity:  O(n)   – each element is processed once
+ * Space Complexity: O(n)   – HashMap stores at most n elements
+ *
+ * This is the optimal solution for the Two Sum problem.
+ */
 
 class Solution {
     public int[] twoSum(int[] MyArray, int target) {
-        for(int i=0;i<MyArray.length;i++){
-            for(int j=i+1;j<MyArray.length;j++){
-                if(MyArray[i]+MyArray[j]==target){
-                    return new int[]{i,j};
-                }
-            }
+      HashMap<Integer,Integer> map = new HashMap<>();
+      for(int i = 0;i<MyArray.length;i++){
+        int complement = target - MyArray[i];
+        if(map.containsKey(complement)){
+            return new int[] {map.get(complement),i};
         }
-        return new int[]{};
+        map.put(MyArray[i],i);
+      }
+      return new int[] {};
     }
 }
