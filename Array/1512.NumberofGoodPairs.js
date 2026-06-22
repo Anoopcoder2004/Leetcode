b475bfff7036574b5366d78d1f5e88c4fb31eff5
Anoop <Anooppopziyan@gmail.com>
2026-06-22 14:33:43 +0530

    1512. Number of Good Pairs

---
+++ Array/1512.NumberofGoodPairs.js
@@ -0,0 +1,18 @@
+// need to optimize solution
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    let goodPairCount = 0;
+    for(let i = 0; i < nums.length - 1; i++){
+        for(let j = i + 1; j < nums.length; j++){
+            if(nums[i] === nums[j] && i < j){
+                goodPairCount++;
+            }
+        }
+    }
+    return goodPairCount;
+    
+};
