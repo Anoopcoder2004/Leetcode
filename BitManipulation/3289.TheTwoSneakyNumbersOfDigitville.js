f2e0557328953a447462680f9e1bca1eade4b3c0
Anoop <Anooppopziyan@gmail.com>
2026-06-25 22:47:25 +0530

    added approach in 3289 The sneaky numbers of digit ville

--- BitManipulation/3289.TheTwoSneakyNumbersOfDigitville.js
+++ BitManipulation/3289.TheTwoSneakyNumbersOfDigitville.js
@@ -1,3 +1,11 @@
+// approach
+
+// create a Map.
+// iterate through all numbers
+// if map does not have the number, add num as key and 1 as value (count); 
+// if map has the key(means it is already seen once), then increment the value of the key by 1.
+// iterate through the map and find the number of values which is 2. and return the count.
+
 /**
  * @param {number[]} nums
  * @return {number[]}
