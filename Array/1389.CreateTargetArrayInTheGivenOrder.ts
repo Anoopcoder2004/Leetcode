2aefcd322d36871410c81319222158fda23cdd04
Anoop <Anooppopziyan@gmail.com>
2026-07-05 20:53:07 +0530

    1389. Create Target Array in the Given Order

---
+++ Array/1389.CreateTargetArrayInTheGivenOrder.ts
@@ -0,0 +1,17 @@
+// Approach
+// do as said in the problem.
+// instead of assigning(=), modify the array using splice operator
+
+function createTargetArray(nums: number[], index: number[]): number[] {
+    const res : number[] = [];
+    for (let i : number = 0; i < nums.length; i++){
+        res.splice(index[i],0,nums[i]);
+    }
+    return res;
+};
+
+// Time complexity
+// O(n)
+
+// Space complexity
+// O(n);
